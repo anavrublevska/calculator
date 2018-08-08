2903bd2764f2f2fdd24d9288c0aa53cdd11bf175
Nastya Vrublevska <nastuniavrublevska@gmail.com>
2018-08-08 13:06:49 +0000

    Initial

---
+++ js/index.js
@@ -0,0 +1,18 @@
+const display = document.querySelector('.calc .display');
+document.querySelectorAll('.calc .num, .calc .op')
+    .forEach( btn => btn.addEventListener('click', numOperClick));
+
+function numOperClick(e) {
+    display.value += e.target.innerText;
+}
+
+document.querySelector('.calc .eq')
+    .addEventListener('click', function(){
+        display.value = eval(display.value);        
+  });
+  
+document.querySelector('.calc .clean')
+    .addEventListener('click', function(){
+        display.value = ' ';        
+  });
+  
